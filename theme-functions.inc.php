59748a59674e1e2400969b5a33b0f8b2a412173d
TheYOSH <theyosh@piratenpartij.nl>
2015-11-08 12:05:29 +0100

    Better cleanup of UTF8 chars in agenda. And added a new option for selecting the calendar items for the newsletter

--- theme-functions.inc.php
+++ theme-functions.inc.php
@@ -1,6 +1,13 @@
 <?php
 
-function get_agenda_items($amount) {
+function clear_agenda_text($string) {
+  $string = trim($string);
+  $string = str_replace(array('â'),'-',$string);
+
+  return $string;
+}
+
+function get_agenda_items($amount = 50) {
     $doc = new DOMDocument();
 	$doc->loadHTML(do_shortcode('[ai1ec view="agenda" events_limit="' . $amount . '"]'));
 	$xpath = new DOMXpath($doc);
@@ -8,27 +15,30 @@ function get_agenda_items($amount) {
 
 	$agenda_list = array();
 	foreach ($agenda_items as $agenda_item) {
-		$agenda_list_item = array('title' => '', 'description' =>'', 'date' => '', 'link' => '');
+		$agenda_list_item = array('title' => '', 'description' =>'', 'date' => '', 'link' => '' , 'id' => -1);
 
 		$agenda_parts = $xpath->query(".//span[@class='ai1ec-event-title']",$agenda_item);
 		foreach ($agenda_parts as $agenda_part) {
-			$agenda_list_item['title'] = trim($agenda_part->nodeValue);
+			$agenda_list_item['title'] = clear_agenda_text($agenda_part->nodeValue);
 		}
 
 		$agenda_parts = $xpath->query(".//div[@class='ai1ec-event-description']",$agenda_item);
 		foreach ($agenda_parts as $agenda_part) {
-			$agenda_list_item['description'] = trim($agenda_part->nodeValue);
+			$agenda_list_item['description'] = clear_agenda_text($agenda_part->nodeValue);
 		}
 
 		$agenda_parts = $xpath->query(".//div[@class='ai1ec-event-time']",$agenda_item);
 		foreach ($agenda_parts as $agenda_part) {
-			$agenda_list_item['date'] = trim($agenda_part->nodeValue);
+			$agenda_list_item['date'] = clear_agenda_text($agenda_part->nodeValue);
 		}
 
 		$agenda_parts = $xpath->query(".//div[@class='ai1ec-btn-group ai1ec-actions']/a",$agenda_item);
 		foreach ($agenda_parts as $agenda_part) {
-			$agenda_list_item['link'] = trim($agenda_part->attributes->getNamedItem('href')->nodeValue);
+			$agenda_list_item['link'] = clear_agenda_text($agenda_part->attributes->getNamedItem('href')->nodeValue);
+			preg_match("/instance_id=(\\d+)/", $agenda_list_item['link'], $matches);
+	                $agenda_list_item['id'] = $matches[1];
 		}
+
 		$agenda_list[] = $agenda_list_item;
 	}
 	return $agenda_list;
@@ -64,10 +74,4 @@ function get_newsletter_item_icon($postid) {
   $thumbnail = trim(ob_get_contents());
   ob_end_clean();
   return $thumbnail;
-/*
-                              if ($thumbnail != '') { ?>
-                                        <img width="125" src="<?php echo $thumbnail; ?>" class="icon" alt="icon">
-                                 <?php }
-                                } ?>
-*/
 }
--- theme-options.php
+++ theme-options.php
@@ -1,42 +1,19 @@
-<table class="form-table">
-    <tr><td colspan="2">General options for header, social links and footer sections could also be set in <a href="?page=newsletter_main_main">Blog Info panel</a>.</td></tr>
-   <tr>
-        <th>Title</th>
-        <td>
-            <?php $controls->text('theme_title'); ?>
-        </td>
-    </tr>
-   <tr>
-        <th>Introduction text</th>
-        <td>
-            <?php $controls->wp_editor('theme_introduction_text'); ?>
-        </td>
-    </tr>
-<!--
-    <tr>
-        <th>Thumbnails</th>
-        <td>
-            <?php $controls->checkbox('theme_thumbnails', 'Add post thumbnails'); ?>
-        </td>
-    </tr>
--->
-  <tr>
-        <th>Lokatie Thumbnails</th>
-        <td>
-            <?php $controls->select('theme_thumbnails_location', array('n' => 'Geen','l' => 'Links' , 'r' => 'Rechts')); ?>
-        </td>
-    </tr>
-    <tr>
-        <th>Aantal agenda items</th>
-        <td>
-            <?php $controls->text('theme_agenda_items'); ?>
-        </td>
-    </tr>
-    <tr>
-        <th>Tags</th>
-        <td>
-            <?php $controls->text('theme_tags', 30); ?>
-            <p class="description" style="display: inline"> comma separated</p>
-        </td>
-    </tr>
-</table>
+<?php
+require_once('theme-functions.inc.php');
+?>
+<p>General options for header, social links and footer sections could also be set in <a href="?page=newsletter_main_main">Blog Info panel</a>.</p>
+<p><strong>Title</strong><br />
+   <?php $controls->text('theme_title',60); ?></p>
+<p><strong>Introduction text</strong><br />
+   <?php $controls->wp_editor('theme_introduction_text'); ?></p>
+<p><strong>Newsletter tag</strong><br />
+   <?php $controls->text('theme_tags', 50); ?></p>
+<p><strong>Location thumbnails</strong><br />
+   <?php $controls->select('theme_thumbnails_location', array('n' => 'None','l' => 'Left' , 'r' => 'Right')); ?></p>
+<p><strong>Agenda items</strong><br />
+<?php
+foreach (get_agenda_items() as $agenda_item) {
+  $controls->checkbox_group('theme_agenda_items',$agenda_item['id'],$agenda_item['date'] . ' - ' . $agenda_item['title']);
+  echo '<br />';
+}
+?></p>
